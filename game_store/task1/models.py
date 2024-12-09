59a68502ae1fc1331b2c671f5e1f96cbf6969fec
Bobakek <165632522+Bobakek@users.noreply.github.com>
2024-12-09 13:07:55 +0400

    Initial commit

---
+++ game_store/task1/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=255, unique=True)  # Имя покупателя (username)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс
+    age = models.PositiveIntegerField()  # Возраст
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)  # Название игры
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Цена
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # Размер файлов
+    description = models.TextField()  # Описание
+    age_limited = models.BooleanField(default=False)  # Ограничение возраста 18+
+    buyers = models.ManyToManyField(Buyer, related_name="games")  # Покупатели
+
+    def __str__(self):
+        return self.title
